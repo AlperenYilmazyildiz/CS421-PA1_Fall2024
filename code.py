32fcbf88e0341f290d5cbfa9bf96eedcada5a09d
Ahmet Alperen Yılmazyıldız <46990789+AlperenYilmazyildiz@users.noreply.github.com>
2024-10-30 12:15:27 +0300

    Upload code.py

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 # CS 421 Programming Assignment 1 - PseudoGit
 
 def greet_the_student():
-    return "12345678"
+    return "123456789123"
 
 if(__name__ == "__main__"):
     print(greet_the_student())
